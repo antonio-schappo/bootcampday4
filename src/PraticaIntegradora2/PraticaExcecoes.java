498c7394e7c4ce4323f3e88cbd07337262bdcd25
Antônio Schappo <antonio.schappo@mercadolivre.com>
2022-03-15 11:43:50 -0300

    Prática Integradora 2 - Aula POO + JAVA II - Exercicio a

---
+++ src/PraticaIntegradora2/PraticaExcecoes.java
@@ -0,0 +1,17 @@
+package PraticaIntegradora2;
+
+public class PraticaExcecoes {
+
+    public static void razao() {
+        int a = 0;
+        int b = 300;
+
+        try {
+            System.out.println(b / a);
+        } catch (ArithmeticException e) {
+            throw new IllegalArgumentException("Nao pode ser dividido por zero");
+        } finally {
+            System.out.println("Programa finalizado");
+        }
+    }
+}
